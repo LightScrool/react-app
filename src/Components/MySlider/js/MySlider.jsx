82b9dd99382d6260c0a278052e34117e668e8c73
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-05-05 19:56:57 +0500

    Deleted TODO line

--- src/Components/MySlider/js/MySlider.jsx
+++ src/Components/MySlider/js/MySlider.jsx
@@ -3,7 +3,6 @@ import '../styles/MySlider.css'
 import MySliderSlide from "./MySliderSlide";
 
 const MySlider = ({data, activeSlide, ANIMATION_DURATION}) => {
-    // TODO: Rewrite on react-transition-group
     let index = 0;
     return (
         <div className="MySlider">
