387f0b76d3fd2f5a682d7be34515287564a889fd
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-05-05 14:43:39 +0500

    Getting rid of 'declare and never used' warnings'

--- src/Pages/SlidersPage.jsx
+++ src/Pages/SlidersPage.jsx
@@ -1,21 +1,18 @@
 import React, {useState} from 'react';
 import "../styles/SlidersPage.css"
 import Container from "../Components/UI/Container/Container";
-import MySliderSlide from "../Components/MySlider/js/MySliderSlide";
 import MySliderDots from "../Components/MySlider/js/MySliderDots";
 import {MySliderData} from '../data';
-import {CSSTransition, SwitchTransition} from "react-transition-group";
 import MySlider from "../Components/MySlider/js/MySlider";
 
 const SlidersPage = () => {
-    const [data, setData] = useState(MySliderData);
     const [activeSlide, setActiveSlide] = useState(0); // Index of active slide
 
     return (
         <div className="SlidersPage">
             <Container>
-                <MySlider data={data} activeSlide={activeSlide}/>
-                <MySliderDots data={data} activeSlide={activeSlide} setActiveSlide={setActiveSlide} />
+                <MySlider data={MySliderData} activeSlide={activeSlide}/>
+                <MySliderDots data={MySliderData} activeSlide={activeSlide} setActiveSlide={setActiveSlide} />
             </Container>
         </div>
     );
