2c123f5f7a6dafcad978c955c642c9faf958106c
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-05-13 20:17:36 +0500

    svg added

--- src/Components/ScrollObject.jsx
+++ src/Components/ScrollObject.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import "../styles/ScrollObject.scss"
 import Container from "./UI/Container/Container";
+import MySVGs from "../assets/MySVGs";
 
 const ScrollObject = () => {
     return (
         <div className="ScrollObject">
             <Container>
                 <div className="ScrollObject__inner">
-                    <h1 className="ScrollObject__sample _onScreen">Sample</h1>
+                    <div className="ScrollObject__svg">
+                        <MySVGs id={"yinYang"} fill="#ffffff"/>
+                    </div>
                 </div>
             </Container>
         </div>
