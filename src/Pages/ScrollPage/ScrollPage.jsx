5b235b0ab9aacbab1031fffc6122d823a266b129
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-05-13 20:28:41 +0500

    event listener created

--- src/Pages/ScrollPage/ScrollPage.jsx
+++ src/Pages/ScrollPage/ScrollPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './ScrollPage.scss';
 import AboutScroll from "../../Components/AboutScroll";
 import ScrollObject from "../../Components/ScrollObject";
 
 const ScrollPage = () => {
+    const onWindowScroll = () => {
+        console.log(1);
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", onWindowScroll)
+
+        return () => window.removeEventListener("scroll", onWindowScroll);
+    }, [])
+
     return (
         <div className="ScrollPage">
             <AboutScroll
