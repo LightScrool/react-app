a69413cb8c464afe7d2620d96afe6e04f28add43
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-05-13 23:06:41 +0500

    positioning now is working on css

--- src/Components/ScrollSector.jsx
+++ src/Components/ScrollSector.jsx
@@ -1,26 +1,14 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef } from 'react';
 import "../styles/ScrollSector.scss"
 import Container from "./UI/Container/Container";
 import MySVGs from "../assets/MySVGs";
-import {getElementTopOffset} from "../utils";
 
 const ScrollSector = () => {
     const block = useRef();
 
-    const [animationStage, setAnimationStage] = useState("_start");
 
     const onWindowScroll = () => {
-        const offset = window.scrollY;
-        const animationStart = getElementTopOffset(block);
-        const animationEnd = animationStart + block.current.getBoundingClientRect().height - window.innerHeight;
-
-        if (animationStart > offset) {
-            setAnimationStage("_start");
-        } else if (animationEnd < offset) {
-            setAnimationStage("_end");
-        } else {
-            setAnimationStage("_extension");
-        }
+        console.log(window.scrollY)
     }
 
     useEffect(() => {
@@ -34,7 +22,7 @@ const ScrollSector = () => {
     return (
         <div ref={block} className="ScrollSector">
             <Container>
-                <div className={`ScrollSector__inner ${animationStage}`}>
+                <div className="ScrollSector__inner">
                     <div className="ScrollSector__svg">
                         <MySVGs id={"yinYang"} fill="#ffffff"/>
                     </div>
