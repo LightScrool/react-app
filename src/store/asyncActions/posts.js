5eb4a1eee45bcb0c6596d7ccd1d8e342d43cd58a
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-05-13 16:12:04 +0500

    fixing async action

--- src/store/asyncActions/posts.js
+++ src/store/asyncActions/posts.js
@@ -4,9 +4,9 @@ import {setPostsAction} from "../postsReducer";
 const POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts";
 
 export const fetchPostsAsyncAction = () => {
-    return function(dispatch) {
-        axios.get(POSTS_DATA_URL)
-            .then(response => response.data)
-            .then(data => dispatch(setPostsAction(data)));
+    return async function (dispatch) {
+        const response = await axios.get(POSTS_DATA_URL);
+        const data = await response.data;
+        dispatch(setPostsAction(data));
     }
 }
