0774bdbc15b24be903dddbac82385b4df690d066
LightScrool <98645665+LightScrool@users.noreply.github.com>
2022-05-14 15:16:21 +0500

    text animation added, working on css classes

--- src/Components/ScrollSector.jsx
+++ src/Components/ScrollSector.jsx
@@ -1,11 +1,18 @@
-import React, {useEffect, useRef } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import "../styles/ScrollSector.scss"
 import Container from "./UI/Container/Container";
 import MySVGs from "../assets/MySVGs";
 
 const ScrollSector = () => {
+    const slideTexts = [
+        'That\'s sample text',
+        'Consectetur adipisicing',
+        'Lorem ipsum dolor',
+        'Other sample text',
+    ]
     const block = useRef();
-    const blockToAnimate = useRef();
+    const blockToRound = useRef();
+    const [active, setActive] = useState(false);
     let animationPercent, startCoords, endCoords;
 
     const calculateAnimationZone = () => {
@@ -23,7 +30,13 @@ const ScrollSector = () => {
 
     const onWindowScroll = () => {
         calculateAnimationPercent();
-        blockToAnimate.current.style.transform = `rotate(${animationPercent*360}deg)`; // TODO: тригерить только при изменении animationPercent
+        const revolutionsQuantity = 1;
+        blockToRound.current.style.transform = `rotate(${revolutionsQuantity*animationPercent*360}deg)`;
+        if (animationPercent >= 0.5){
+            setActive(true)
+        } else {
+            setActive(false)
+        }
     }
 
     // ditMount
@@ -39,13 +52,28 @@ const ScrollSector = () => {
         }
     }, [])
 
+
+
     return (
         <div ref={block} className="ScrollSector">
             <Container>
                 <div className="ScrollSector__inner">
-                    <div className="ScrollSector__svg" ref={blockToAnimate}>
+                    <div className="ScrollSector__svg" ref={blockToRound}>
                         <MySVGs id={"yinYang"} fill="#ffffff"/>
                     </div>
+                    {
+                        slideTexts.map((text, index) => {
+                            return(
+                                <div
+                                    key={index}
+                                    id={'slide-text-' + (index)}
+                                    className={"ScrollSector__slide-text" + (active ? " _active" : "")}
+                                >
+                                    {text}
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </Container>
         </div>
